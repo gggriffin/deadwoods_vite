40e2fd041f4e965858b2690b419e7d9bf9dae09f
Garrett Griffin <gggriffin@Garretts-MacBook-Pro.local>
2024-08-25 09:04:42 -0500

    chore: checking removing background path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 //TODO: Start with finishing static-deploy guide
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
 })
