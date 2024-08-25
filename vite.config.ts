082e37e18cc9fe51c2d00a27ce1ade9fb6777b11
Garrett Griffin <gggriffin@Garretts-MacBook-Pro.local>
2024-08-25 11:38:10 -0500

    chore: removed workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-
 //TODO: Start with finishing static-deploy guide
 // https://vitejs.dev/config/
 export default defineConfig({
