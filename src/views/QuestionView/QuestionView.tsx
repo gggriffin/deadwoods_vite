d6539fa5b536b75dfac134222b0751ba2754c367
Garrett Griffin <gggriffin@Garretts-MacBook-Pro.local>
2024-08-25 22:53:01 -0500

    chore: add container to ImageCarousel

--- src/views/QuestionView/QuestionView.tsx
+++ src/views/QuestionView/QuestionView.tsx
@@ -5,7 +5,7 @@ import { QuestionAsked } from "./QuestionAsked";
 
 export const QuestionView = () => {
   return (
-    <div className='h-full flex flex-col lg:items-center'>
+    <div className="h-full flex flex-col lg:items-center">
       <InfoHeaderText>Admission </InfoHeaderText>
       <InfoTile>
         Tickets are $10 per person. Children ages 10 and under are free. Tickets
@@ -20,11 +20,13 @@ export const QuestionView = () => {
         local Christmas drive and provided access to eyecare and other
         necessities for children in our communities.
       </InfoTile>
-      <ImageCarousel />
+      <div className="w-100 flex flex-grow justify-center">
+        <ImageCarousel />
+      </div>
       <InfoHeaderText>Frequently Asked Questions</InfoHeaderText>
       <InfoTile>
         <QuestionAsked>Is The Deadwoods open rain or shine?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           No. This attraction is entirely outdoors, and therefore will be
           weather dependent. Light rain or cold weather will not affect this
           event but heavy downpours or other extreme conditions may. Check here
@@ -34,14 +36,14 @@ export const QuestionView = () => {
       </InfoTile>
       <InfoTile>
         <QuestionAsked>What is the best time to arrive?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           Gates open at 7:00pm, with the first groups entering the attraction at
           sundown.
         </div>
       </InfoTile>
       <InfoTile>
         <QuestionAsked>Are there any age restrictions?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           We recommend that children under the age of 14 be acommpanied by an
           adult. The attraction is intended to be frightening, so young children
           may become upset or scared. You know your child better than anyone -
@@ -50,7 +52,7 @@ export const QuestionView = () => {
       </InfoTile>
       <InfoTile>
         <QuestionAsked>What is the parking situation?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           There is ample parking available with no extra cost. Vehicles will be
           parked on flat, grassy terrain - not on pavement or gravel. Keep this
           in mind if the ground is soggy from rain.
@@ -58,13 +60,13 @@ export const QuestionView = () => {
       </InfoTile>
       <InfoTile>
         <QuestionAsked>Will there be restrooms available?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           Yes, we will provide portable toilets for public use.
         </div>
       </InfoTile>
       <InfoTile>
         <QuestionAsked>Can tickets be purchased in advance?</QuestionAsked>
-        <div className='text-sm'>
+        <div className="text-sm">
           No, tickets must be purchased on site the night of the event.
         </div>
       </InfoTile>
