db5c9918ef243153b77afacdb3b75772538278e8
Garrett Griffin <gggriffin@Garretts-MacBook-Pro.local>
2024-08-25 20:36:24 -0500

    chore: enhance vite.config build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
+});
