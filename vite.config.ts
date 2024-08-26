2e5c7cffacbaef871b52a2c6fcf806d8607d6c4b
Garrett Griffin <gggriffin@Garretts-MacBook-Pro.local>
2024-08-25 21:51:30 -0500

    chore: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
 });
